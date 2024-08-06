d4e850af0752c82915f5a068ec90baa88e76eba1
ivan-bilenkyi <ivanbilenkyi@gmail.com>
2024-08-06 05:22:43 +0300

    delete all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import FirstPage from 'pages/FirstPage/FirstPage';
-import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
 
 const test = import.meta.env.VITE_API_TEST;
@@ -15,10 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </AppWrapper>
--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,11 +1,9 @@
 import { Container, Title, StyledImage } from './FirstPage.styled';
-import example from '../../assets/example.png';
 
 const FirstPage = () => {
   return (
     <Container>
       <Title>First Page</Title>
-      <StyledImage src={example} alt="Example" />
     </Container>
   );
 };
